2937b1f070a70933eeddf196ae3a987bbe35d514
Osayi Eseosa <osayivictoryeseosa@gmail.com>
2023-12-30 17:52:10 +0100

    bug fixes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'https://norbilefoodsapi.vercel.app'
+const BASE_URL = 'https://norbileapi.vercel.app'
 
 export default axios.create({
     baseURL: BASE_URL
